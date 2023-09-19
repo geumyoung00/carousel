3361024d910782c4acd0b96073fc2225714d8469
geumyoung <1gyn093@gmail.com>
2023-09-19 23:16:03 +0900

    carousel 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
+
+const images = [
+  "https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+];
 
 function App() {
+  const [imgIdx, setImgIdx] = useState(0);
+  const last = images.length - 1;
+  const arwHandler = (direction) => {
+    if (direction === "left") {
+      if (imgIdx === 0) {
+        setImgIdx(last);
+      } else {
+        setImgIdx(imgIdx - 1);
+      }
+    }
+    if (direction === "right") {
+      if (imgIdx === last) {
+        setImgIdx(0);
+      } else {
+        setImgIdx(imgIdx + 1);
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Project 1: Carousel</h2>
+        <div className="slider">
+          <div className="left-arrow" onClick={() => arwHandler("left")}>
+            ⬅
+          </div>
+          <div className="right-arrow" onClick={() => arwHandler("right")}>
+            ⮕
+          </div>
+        </div>
+        <div className="slide">
+          <img src={images[imgIdx]} />
+        </div>
+      </div>
     </div>
   );
 }
